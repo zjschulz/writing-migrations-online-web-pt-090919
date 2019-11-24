a5bf448a2a6a36cce25f502b256483962e94412a
Zachary Schulz <zjschulz@gmail.com>
2019-11-24 02:43:11 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,7 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   
   def change
-    change_column :students do |t|
-      t.datetime :birthdate
+    change_column(:students, :birthdate, :datetime)
     end
   end
   
